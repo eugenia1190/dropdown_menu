c49c06a81cdd24af8efcd32c441c595a91e2924f
Evgenia Petukhova <chet444@mail.ru>
2020-03-11 23:32:25 +0500

    change design for menu item

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const menuItem = [...document.getElementsByClassName('menu__item')];
 const submenu = [...document.getElementsByClassName('submenu')];
-
+const menuItemTitle = [...document.getElementsByClassName('menu__item-title')];
 menuItem.forEach(function(item, i) {
 	item.addEventListener('mouseover' , event => {
-		submenu[i].classList.add('submenu-active'); 
+		submenu[i].classList.add('submenu-active');
+		menuItemTitle[i].classList.add('menu__item-title-active');
 		submenu[i].addEventListener('mouseout', event => {
         	submenu[i].classList.remove('submenu-active');
+        	menuItemTitle[i].classList.remove('menu__item-title-active');
     	});
 	});
 });
